8ccc4ef468cf635e94133fd892b7fadb935636e9
ticktock18 <issaselemanimgogo@gmail.com>
2025-08-26 18:53:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "mimihapa-cache-v1";
 const urlsToCache = [
   "/",
-  "https://ticktock18.github/internet-bundles/manifest.json",
+  "https://ticktock18.github.io/internet-bundles/manifest.json",
   "https://mimihapa.co.tz/favicon.ico"
 ];
 
@@ -18,4 +18,5 @@ self.addEventListener("fetch", event => {
     .then(response => response || fetch(event.request))
   );
 });
+
 
